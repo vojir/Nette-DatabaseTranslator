e110893ba2461e76cdaaf427762a63d190000c76
Stanislav Vojíř <stanislav.vojir@gmail.com>
2021-01-26 16:57:36 +0100

    update pro novější verze závislostí

--- src/ITranslator.php
+++ src/ITranslator.php
@@ -9,7 +9,7 @@ namespace Vojir\DatabaseTranslator;
  * @author Stanislav Vojíř
  * @license http://www.apache.org/licenses/LICENSE-2.0 Apache License, Version 2.0
  */
-interface ITranslator extends \Nette\Localization\ITranslator{
+interface ITranslator extends \Nette\Localization\Translator{
 
   /**
    * Method returning the actual language
